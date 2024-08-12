a6b3e995a018dbf07dda2731b0c33811489b77cc
Ashok <varunkumarkmvarunkumarkm19@gmail.com>
2024-08-12 19:21:57 +0530

    some changes

--- Demo.java
+++ Demo.java
@@ -2,16 +2,28 @@ class Demo
 {
 	Public static void main(String[] args)
 	{
+<<<<<<< HEAD
+=======
+<<<<<<< Updated upstream
+>>>>>>> parent of 2efbe6e (Update Demo.java)
 		// task 321
 		void m2()
 		{
 			//logic 
+<<<<<<< HEAD
 
+=======
+=======
+>>>>>>> parent of 2efbe6e (Update Demo.java)
 		//task 320
 		public void m1()
 		{
 			//logic
+<<<<<<< HEAD
 
+=======
+>>>>>>> Stashed changes
+>>>>>>> parent of 2efbe6e (Update Demo.java)
 		}
 	}
 }
