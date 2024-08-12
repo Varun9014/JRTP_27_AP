32d86a4987cde871fbd17da83c51da398cdb4178
Ashok <varunkumarkmvarunkumarkm19@gmail.com>
2024-08-12 19:10:40 +0530

    Revert "Update Demo.java"
    This reverts commit 2efbe6eadf87addc791f9ea3060dded1a547eba1.

--- Demo.java
+++ Demo.java
@@ -2,17 +2,17 @@ class Demo
 {
 	Public static void main(String[] args)
 	{
-		 Updated upstream
+<<<<<<< Updated upstream
 		// task 321
 		void m2()
 		{
 			//logic 
- 
+=======
 		//task 320
 		public void m1()
 		{
 			//logic
-	 
+>>>>>>> Stashed changes
 		}
 	}
 }
