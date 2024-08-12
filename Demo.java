798b32f65a48b4dfaf7eb09b699d197a962dc03e
Ashok <varunkumarkmvarunkumarkm19@gmail.com>
2024-08-12 19:12:07 +0530

    Reapply "g3"
    This reverts commit 6df78231f460641aee733159c84d77ab1de72ce4.

--- Demo.java
+++ Demo.java
@@ -2,17 +2,16 @@ class Demo
 {
 	Public static void main(String[] args)
 	{
-<<<<<<< Updated upstream
 		// task 321
 		void m2()
 		{
 			//logic 
-=======
+
 		//task 320
 		public void m1()
 		{
 			//logic
->>>>>>> Stashed changes
+
 		}
 	}
 }
