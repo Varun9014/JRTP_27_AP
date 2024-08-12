6df78231f460641aee733159c84d77ab1de72ce4
Ashok <varunkumarkmvarunkumarkm19@gmail.com>
2024-08-12 19:02:41 +0530

    Revert "g3"
    This reverts commit b32a8ac7fe3632afa5d49d8845efe1cca7c5afc6.

--- Demo.java
+++ Demo.java
@@ -2,16 +2,17 @@ class Demo
 {
 	Public static void main(String[] args)
 	{
+<<<<<<< Updated upstream
 		// task 321
 		void m2()
 		{
 			//logic 
-
+=======
 		//task 320
 		public void m1()
 		{
 			//logic
-
+>>>>>>> Stashed changes
 		}
 	}
 }
